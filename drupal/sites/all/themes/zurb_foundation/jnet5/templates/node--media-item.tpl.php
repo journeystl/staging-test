a4a8b3bc967355066e0e456da66954a2129717fb
Kraig Hufstedler <kraigory@gmail.com>
2012-08-03 03:36:32 -0500

    played around with node--media-item.tpl.php

---
+++ drupal/sites/all/themes/zurb_foundation/jnet5/templates/node--media-item.tpl.php
@@ -0,0 +1,94 @@
+<?php
+
+/**
+ * STATIC CODE
+ */
+?>
+
+<h1> <?php print($title); ?>
+<h5>BY <?php print($node->field_speaker); ?>
+
+
+<div class="row">
+	<div class="twevlve columns">
+		<p><img src="http://placehold.it/1200x500&text=sermon+clip+about+Jesus"></p>
+		<p class="lead">We are a people who love Jesus, have had our lives made brand new by him, and want others to meet him too. We believe Jesus is our:</p>
+	</div>
+</div>
+	
+	
+<div class="row">
+	<div id="savior" class="six columns">
+		<h2>Living Savior</h2>
+		<p>Jesus is the Living Savior, sent by God to overcome the separation between God and man caused by sin.  We believe that Jesus lived an earthly life free of sin, dying on the cross to ultimately atone for the sins of all mankind;  that he rose again on the third day as testified to by numerous accounts and eye-witnesses, and that he is alive and at work today through his Holy Spirit, sent to lead us into all truth.  He is the Living Savior in that he is actively drawing people to himself to this day with the free offer of forgiveness to anyone who will believe in and trust in him.
+</p>
+	</div>
+	<div id="lord" class="six columns">
+		<h2>Risen Lord</h2>
+		<p>When Jesus rose from the dead, he appeared to many, bearing witness to the truth of his testimony.  When we say he is the Risen Lord, we acknowledge that his resurrection was the greatest miracle in human history, and that through it those who believe in him are brought into a state of peace with God.  
+</p>
+	</div>
+</div>
+
+<div class="row">
+	<div id="friend" class="six columns">
+		<h2>Friend</h2>
+		<p>We believe that Jesus is available to all that would seek him.  The Bible teaches that he loves us unconditionally and desires fellowship with us.  He asks us to trust him, to cast our anxieties on him, and to seek him for direction in our lives.  He promises to guide us through the seasons of life, never leaving nor forsaking us. The Bible say that when we believe, he begins a process of transformation within us, making us more and more like him and even allowing us to "bear fruit" for him as we become his disciples; reflecting his love and living out his specific will for us. 
+
+</p>
+	</div>
+	<div id="god" class="six columns">
+		<h2>God</h2>
+		<p>Jesus is, and always has been God.  According to the Bible, he was with God and he was God before the Earth was even created.   He was "begotten", not conceived, and though fully man while on this earth, he never ceased to also fully be God.  To truly atone for our sins, he had to bear the weight of flesh, temptation, and suffering; thus making him a God who is able to sympathize with us in our weaknesses, and intercede on our behalf.</p>
+	</div>
+</div>
+
+<div class="row margin-bottom-med">
+	<div class="span12">
+		<hr>
+	</div>
+	<div id="learn" class="six columns">
+		<h2>Learn More</h2>
+		<p>If you don't know who Jesus is, we encourage you to read more about him in the New Testament gospels (LINK), which detail his life and earthly ministry.   Additional resources that we highly recommend include:</p>
+		<div class="row">
+			<div class="three columns">
+				<ul class="nav nav-list well margin-bottom-small">
+					<li class="nav-header"><strong>Books</strong></li>
+					<li><a href="#">Mere Christianity by C.S. Lewis</a></li>
+					<li><a href="#">Reason for God by Tim Keller</a></li>
+				</ul>
+			</div>
+			<div class="three columns">
+				<ul class="nav nav-list well margin-bottom-small">
+					<li class="nav-header"><strong>Bible Reading Plan</strong></li>
+					<li><a href="#">Engage Scripture</a></li>
+					<li class="nav-header"><strong>Sermons and Videos</strong></li>
+					<li><a href="#">Jesus & sermon series</a></li>
+				</ul>
+			</div>
+		</div>
+		
+				
+		
+		
+		
+
+	</div>
+	<div id="visit" class="six columns">
+		<h2>Vist a Church</h2>
+		<p>Attend one of our churches and meet us at Connection Point to learn more about Jesus and get your questions answered.</p>
+		  	<div class="btn-group">
+		    	<a class="btn dropdown-toggle btn-large btn-primary" data-toggle="dropdown" href="#">
+		        		Visit A Church
+		        	<b class="caret"></b>
+		      	</a>
+		    	<ul class="dropdown-menu">
+		    		<!-- links -->
+		    		<li><a href="/tg/visit">Tower Grove</a></li>
+		    		<li><a href="/hr/visit">Hanley Road</a></li>
+		    		<li><a href="/wc/visit">West County</a></li>
+		    		<li><a href="/bl/visit">Belleville</a></li>
+		    	</ul>
+		  	</div>
+	</div>
+</div>
