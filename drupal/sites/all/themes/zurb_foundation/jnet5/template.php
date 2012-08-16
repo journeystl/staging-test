b41dd98829081b9a58ba10f073a527e250e061f8
Charlie <charlie@csdesignco.com>
2012-08-16 10:51:21 -0500

    Added media series CSS and background image in template_preprocess_html().

--- drupal/sites/all/themes/zurb_foundation/jnet5/template.php
+++ drupal/sites/all/themes/zurb_foundation/jnet5/template.php
@@ -25,6 +25,16 @@ function jnet5_preprocess(&$vars, $hook) {
  * Impements template_preprocess_html().
  */
 function jnet5_preprocess_html(&$vars) {
+  // Add the background image on media_series nodes.
+  if (isset($vars['menu_item']['page_arguments']) && isset($vars['menu_item']['page_arguments'][0]->type)) {
+    // Attach node--media-series.css
+    drupal_add_css(drupal_get_path('theme', 'jnet5') . '/css/node--media-series.css');
+    // Check if a background image is attached and set it to the body's background.
+    if ($vars['menu_item']['page_arguments'][0]->type == 'media_series' && isset($vars['menu_item']['page_arguments'][0]->field_background_image[LANGUAGE_NONE])) {
+      $bg_image = file_create_url($vars['menu_item']['page_arguments'][0]->field_background_image[LANGUAGE_NONE][0]['uri']);
+      $vars['attributes_array']['style'] = "background: url({$bg_image});";
+    }
+  }
 }
 
 /**
