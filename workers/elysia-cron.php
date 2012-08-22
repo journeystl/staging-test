19ed5368fbbf9e3518d3f348e5a0a33c0efc14da
Charlie <charlie@csdesignco.com>
2012-08-22 12:00:32 -0500

    ...

--- workers/elysia-cron.php
+++ workers/elysia-cron.php
@@ -3,6 +3,8 @@
 // Bootstrap Drupal
 define("DRUPAL_ROOT", dirname(__FILE__) . '/../drupal');
 $_SERVER['REMOTE_ADDR'] = '127.0.0.1';
+$_SERVER['REQUEST_METHOD'] = 'GET';
+$_SERVER['SERVER_SOFTWARE'] = 'Apache/2.2.16 (Ubuntu)';
 require_once dirname(__FILE__) . '/../drupal/includes/bootstrap.inc';
 drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
 
