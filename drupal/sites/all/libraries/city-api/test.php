2eab2dfb91868c20cd05e77a7c5e89f16aecaa27
Jason Froderman <jason@thecatena.com>
2012-08-23 11:51:08 -0500

    Testing City API ... round 1

--- drupal/sites/all/libraries/city-api/test.php
+++ drupal/sites/all/libraries/city-api/test.php
@@ -1,6 +1,10 @@
 <?php
-      require_once(dirname(__FILE__) . '/lib/ca-main.php');
-      $ca = new CityApi();
-      $results = $ca->groups_count();
-      echo $results;
+    	require_once(dirname(__FILE__) . '/lib/ca-main.php');
+
+    	$ca = new CityApi();
+    	$ca->debug = true;
+		$ca->json = true;
+
+		$results = $ca->groups_index(array('group_types' => "CG")); 
+    	echo $results;
 ?>
