eb1a84823174e5480b0c3a09ab5fada39756e7d7
Kraig Hufstedler <kraigory@gmail.com>
2012-09-10 14:32:59 -0500

    fixed again

--- drupal/sites/all/themes/jnet5/templates/node/node--promotion-content--topic.tpl.php
+++ drupal/sites/all/themes/jnet5/templates/node/node--promotion-content--topic.tpl.php
@@ -1,15 +1,3 @@
-
 <div id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?>"<?php print $attributes; ?>>
-
-    <hr>
     <div><?php print render($content['body']); ?></div>
-	
-
-
-
-	
-	
-<!--    <?php print render($content); ?>-->
-  
-
 </div>
