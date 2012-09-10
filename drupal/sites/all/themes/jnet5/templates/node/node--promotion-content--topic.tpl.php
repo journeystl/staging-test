d70dc6c2ab215d670445e7f8d5efee0ee49508ae
Kraig Hufstedler <kraigory@gmail.com>
2012-09-10 14:29:56 -0500

    fixed topic tpl

--- drupal/sites/all/themes/jnet5/templates/node/node--promotion-content--topic.tpl.php
+++ drupal/sites/all/themes/jnet5/templates/node/node--promotion-content--topic.tpl.php
@@ -1,12 +1,6 @@
-TOPIC
-
 
 <div id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?>"<?php print $attributes; ?>>
 
-  
-    <h1<?php if($title_attributes)print $title_attributes; ?>><?php if($title)print $title; ?></h1>
-    <h6>By <?php print render($content['field_author_longname'][0]['#markup']); ?></h6>
-    <span class="label secondary radius"><?php print render(ucfirst($content['field_type'][0]['#markup'])); ?></span>
     <hr>
     <div><?php print render($content['body']); ?></div>
 	
