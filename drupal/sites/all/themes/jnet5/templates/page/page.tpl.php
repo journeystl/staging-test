38c1fba14c547b6e6281df074bcf4d82690a7642
Charlie <charlie@csdesignco.com>
2012-09-10 15:12:25 -0500

    Commented out feeds http request cache.

--- drupal/sites/all/themes/jnet5/templates/page/page.tpl.php
+++ drupal/sites/all/themes/jnet5/templates/page/page.tpl.php
@@ -10,24 +10,13 @@
   <?php print $top_churches_bar; ?>
   <div class="row">
     <?php print render($page['header']); ?>
+  </div>
+  <div class="row">
     <div id="header" class="three columns centered">
-      	<p>
-        	<a href="/">
-        		<img src="/<?php print path_to_theme(); ?>/images/navigation/JourneyLogo_05.png">
-        	</a>
-        </p>
+      <a href="<?php print $GLOBALS['base_path']; ?>"><img src="<?php print $GLOBALS['base_path'] . $GLOBALS['theme_path']; ?>/images/navigation/logo_big.gif"></a>
     </div>
-
-
   </div>
 </div>
-
-
-
-<?php //if (!empty($page['content_header'])): print render($page['content_header']); endif; ?>
-
-
-
 
 <div class="row">
   <?php if ($messages): print $messages; endif; ?>
