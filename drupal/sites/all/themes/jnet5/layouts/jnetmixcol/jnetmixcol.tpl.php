8f27f70d05cc5d491839c520951ea266410f1a34
Simon <syost@journeyon.net>
2012-09-11 14:10:15 -0500

    trial of row-inner

--- drupal/sites/all/themes/jnet5/layouts/jnetmixcol/jnetmixcol.tpl.php
+++ drupal/sites/all/themes/jnet5/layouts/jnetmixcol/jnetmixcol.tpl.php
@@ -26,6 +26,7 @@
 	
 	<?php if($content['left1'] or $content['right1']): ?>
 		<div class="row">
+		<div class="row-inner">
 		  	<div class="six columns">
 		    	<?php print $content['left1']; ?>
 		  	</div>
@@ -33,6 +34,7 @@
 		  	<div class="six columns">
 		    	<?php print $content['right1']; ?>
 		  	</div>
+		</div>
 	  	</div>
 	 <?php endif; ?>
 	 
