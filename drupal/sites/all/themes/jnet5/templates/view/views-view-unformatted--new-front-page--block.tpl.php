5448fc122f98f446e05021638ddb7f3be7224ac7
Charlie <charlie@csdesignco.com>
2012-09-11 15:21:08 -0500

    Orbit theme work.

--- drupal/sites/all/themes/jnet5/templates/view/views-view-unformatted--new-front-page--block.tpl.php
+++ drupal/sites/all/themes/jnet5/templates/view/views-view-unformatted--new-front-page--block.tpl.php
@@ -9,14 +9,13 @@
 
 <div class="row">
 	<h2 class="two columns"><i class="g-foundicon-refresh"></i> News</h2>
-	<div class="ten columns">	
+	<div class="ten columns">
 		<div id="News">
 		<?php foreach ($rows as $id => $row): ?>
 		  <div class="<?php print $classes_array[$id]; ?>">
 		    <?php print $row; ?>
 		  </div>
 		<?php endforeach; ?>
-		<div class="slider-nav"><span class="right">Right</span><span class="left">Left</span></div>
 		</div>
 	</div>
 
@@ -24,29 +23,25 @@
 
 <script type="text/javascript">
 (function ($) {$(document).ready(function() {
-   $(window).load(function() {
-       $('#News').orbit({
-			animation: 'horizontal-push',   // fade, horizontal-slide, vertical-slide, horizontal-push
-			animationSpeed: 800,                // how fast animtions are
-			timer: true, 			 // true or false to have the timer
-			resetTimerOnClick: false,           // true resets the timer instead of pausing slideshow progress
-			advanceSpeed: 2000, 		 // if timer is enabled, time between transitions 
-			pauseOnHover: true, 		 // if you hover pauses the slider
-			startClockOnMouseOut: true, 	 // if clock should start on MouseOut
-			startClockOnMouseOutAfter: 500, 	 // how long after MouseOut should the timer start again
-			directionalNav: true, 		 // manual advancing directional navs
-			captions: true, 			 // do you want captions?
-			captionAnimation: 'fade', 		 // fade, slideOpen, none
-			captionAnimationSpeed: 800, 	 // if so how quickly should they animate in
-			bullets: false,			 // true or false to activate the bullet navigation
-			bulletThumbs: false,		 // thumbnails for the bullets
-			bulletThumbLocation: '',		 // location from this file where thumbs will be
-			afterSlideChange: function(){}, 	 // empty function 
-			fluid: '25x1'                         // or set a aspect ratio for content slides (ex: '4x3') 
-       });
-   });
-   
-
-   
+  $(window).load(function() {
+    $('#News').orbit({
+			animation: 'horizontal-push', // fade, horizontal-slide, vertical-slide, horizontal-push
+			animationSpeed: 800, // how fast animtions are
+			timer: false, // true or false to have the timer
+			resetTimerOnClick: false, // true resets the timer instead of pausing slideshow progress
+			advanceSpeed: 2000, // if timer is enabled, time between transitions
+			pauseOnHover: true, // if you hover pauses the slider
+			startClockOnMouseOut: true, // if clock should start on MouseOut
+			startClockOnMouseOutAfter: 500, // how long after MouseOut should the timer start again
+			directionalNav: true, // manual advancing directional navs
+			captions: false, // do you want captions?
+			captionAnimation: 'fade', // fade, slideOpen, none
+			captionAnimationSpeed: 800, // if so how quickly should they animate in
+			bullets: false, // true or false to activate the bullet navigation
+			bulletThumbs: false, // thumbnails for the bullets
+			bulletThumbLocation: '', // location from this file where thumbs will be
+			fluid: '25x1' // or set a aspect ratio for content slides (ex: '4x3')
+    });
+  });
 });})(jQuery);
 </script>
