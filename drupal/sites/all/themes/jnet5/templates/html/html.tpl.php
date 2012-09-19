1e21e11c87a84d131a4572bd6b98d7423c4b0417
Simon <syost@journeyon.net>
2012-09-19 10:43:35 -0500

    top menu

--- drupal/sites/all/themes/jnet5/templates/html/html.tpl.php
+++ drupal/sites/all/themes/jnet5/templates/html/html.tpl.php
@@ -21,7 +21,7 @@
 	<![endif]-->
 </head>
 <body class="<?php print $classes; ?>" <?php print $attributes;?>>
-  <div class="shim hide-for-small"></div>
+  
   <div class="skip-link">
     <a href="#main-content" class="element-invisible element-focusable"><?php print t('Skip to main content'); ?></a>
   </div>
