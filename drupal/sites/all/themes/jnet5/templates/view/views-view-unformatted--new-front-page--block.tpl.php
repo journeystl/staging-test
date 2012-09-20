a7e8eb3908af138f4ca737bc220fd9d2be06a049
Simon <syost@journeyon.net>
2012-09-20 09:48:05 -0500

    header

--- drupal/sites/all/themes/jnet5/templates/view/views-view-unformatted--new-front-page--block.tpl.php
+++ drupal/sites/all/themes/jnet5/templates/view/views-view-unformatted--new-front-page--block.tpl.php
@@ -1,13 +1,10 @@
 <?php
 /**
- * @file views-view-unformatted.tpl.php
- * Default simple view template to display a list of rows.
- *
- * @ingroup views_templates
+ * NEWS
  */
 ?>
 
-<div class="row">
+<div class="row hide-for-medium-down">
 	<h2 class="two columns"><i class="g-foundicon-refresh"></i> News</h2>
 	<div class="ten columns">
 		<div id="News">
@@ -29,7 +26,7 @@
 			animationSpeed: 800, // how fast animtions are
 			timer: true, // true or false to have the timer
 			resetTimerOnClick: false, // true resets the timer instead of pausing slideshow progress
-			advanceSpeed: 2000, // if timer is enabled, time between transitions
+			advanceSpeed: 5000, // if timer is enabled, time between transitions
 			pauseOnHover: true, // if you hover pauses the slider
 			startClockOnMouseOut: true, // if clock should start on MouseOut
 			startClockOnMouseOutAfter: 500, // how long after MouseOut should the timer start again
