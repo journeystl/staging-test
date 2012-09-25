245baaa326275f14c2bed5abd92e9be62d1af94e
Charlie <charlie@csdesignco.com>
2012-09-25 08:57:24 -0500

    ...

--- drupal/sites/all/themes/jnet5/templates/node/node--promotion-content--topic.tpl.php
+++ drupal/sites/all/themes/jnet5/templates/node/node--promotion-content--topic.tpl.php
@@ -1,6 +1,5 @@
 <div id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?>"<?php print $attributes; ?>>
 	<?php print jnet5_add_this(); ?>
 	<?php print render($content['body']); ?>
-	<?php print jnet5_add_this(); ?>
 </div>
 
