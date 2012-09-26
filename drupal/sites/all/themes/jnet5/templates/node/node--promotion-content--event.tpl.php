085272d66e2acaf6dcaf92747e01a6b8e95ee8ae
Simon <syost@journeyon.net>
2012-09-26 14:42:41 -0500

    table styles

--- drupal/sites/all/themes/jnet5/templates/node/node--promotion-content--event.tpl.php
+++ drupal/sites/all/themes/jnet5/templates/node/node--promotion-content--event.tpl.php
@@ -1,6 +1,6 @@
 <div id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?>"<?php print $attributes; ?>>
   <hr class="top">
   <?php print render($content['body']); ?>
-  <hr class="top">
+  <hr class="bottom">
   <?php print jnet5_add_this(); ?>
 </div>
