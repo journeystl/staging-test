c643f56f125b27827dae68f60ec74f65ab007a0a
Simon <syost@journeyon.net>
2012-09-25 21:00:40 -0500

    event tpl and css

--- drupal/sites/all/themes/jnet5/templates/block/block--block--4.tpl.php
+++ drupal/sites/all/themes/jnet5/templates/block/block--block--4.tpl.php
@@ -16,13 +16,29 @@ $campus_name = ($campus_id) ? jnet5_campus_name_by_pbid($campus_id[0]['value']) 
 	<div class="eight columns">
 		<h1><?php print render($node->title); ?></h1>
 	</div> <!--/.eight columns-->
+	
 	<div class="four columns" id="event-rsvp">
 		<a href="<?php print render($node->field_short_url[$node->language][0]['value']); ?>" class="button radius large">RSVP  <span class="label round"><?php print count($node->field_event_responses); ?></span></a>
 		<a href="http://maps.google.com?q=<?php print render($node->field_event_address_street[$node->language][0]['value']); ?> <?php print render($node->field_event_address_city[$node->language][0]['value']); ?> <?php print render($node->field_event_address_state[$node->language][0]['value']); ?> <?php print render($node->field_event_address_zip[$node->language][0]['value']); ?>" class="button secondary radius large" id="event-map-btn">Map It</a>
 	</div> <!--/.four columns-->
+	
 </div> <!--/.row-->
+	
 <div class="row">
+
 	<div class="eight columns">
+		<h3><?php print render(field_view_field('node', $node, 'field_event_starting_at')); ?></h3>
+	</div> <!--/.eight columns-->
+
+	<div class="four columns">
+		<div class="location-link"><strong>Location:</strong> <a href="http://maps.google.com?q=<?php print render($node->field_event_address_street[$node->language][0]['value']); ?> <?php print render($node->field_event_address_city[$node->language][0]['value']); ?> <?php print render($node->field_event_address_state[$node->language][0]['value']); ?> <?php print render($node->field_event_address_zip[$node->language][0]['value']); ?>" class="" id="event-map-link"><?php print render($node->field_event_address_street[$node->language][0]['value']); ?>, <?php print render($node->field_event_address_city[$node->language][0]['value']); ?>, <?php print render($node->field_event_address_state[$node->language][0]['value']); ?> <?php print render($node->field_event_address_zip[$node->language][0]['value']); ?></a></div>
+	</div> <!--/.four columns-->
+
+</div> <!--/.row-->
+
+<div class="row">
+
+	<div class="twelve columns">
 		<?php if ($node->field_author_longname): ?>
 			<h5>By <?php print render($node->field_author_longname[$node->language][0]['value']); ?></h5>
 		<?php endif; ?>
@@ -34,14 +50,5 @@ $campus_name = ($campus_id) ? jnet5_campus_name_by_pbid($campus_id[0]['value']) 
 		<?php endif; ?>
 	</div> <!--/.eight-->
 
-	<div class="four columns" id="event-deets">
-		<div><?php print render(field_view_field('node', $node, 'field_event_starting_at')); ?></div><br>
-		<div><strong>Location:</strong></div>
-		<div><?php print render($node->field_event_address_street[$node->language][0]['value']); ?></div>
-		<div><?php print render($node->field_event_address_city[$node->language][0]['value']); ?>
-		<?php print render($node->field_event_address_state[$node->language][0]['value']); ?>
-		<?php print render($node->field_event_address_zip[$node->language][0]['value']); ?></div>
-		
-	</div> <!--/.four columns-->
 
 </div> <!--/.row-->
