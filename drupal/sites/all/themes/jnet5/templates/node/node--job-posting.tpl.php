ad001d0260f33aa8de096af1caa0be9cc408e8f6
Jason Froderman <jason@thecatena.com>
2012-09-27 06:12:35 -0500

    Job posting changes

---
+++ drupal/sites/all/themes/jnet5/templates/node/node--job-posting.tpl.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * NODE
+ */
+?>
+
+<div class="row">
+	<div class="eight columns">
+		<div id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?>"<?php print $attributes; ?>>
+			<?php print render($title_prefix); ?>
+			<h2<?php print $title_attributes; ?>><?php print $title; ?></h2>
+			<?php print render($title_suffix); ?>
+
+			<?php
+				// We hide the comments and links now so that we can render them later.
+				// hide($content['comments']);
+				// hide($content['links']);
+				// hide($content['field_tags']);
+				// print render($content);
+				print render($content['body']);
+				// dpm($content);
+			?>
+		</div>
+	</div>
+	<div class="four columns">
+		<h3>Application Requirements</h3>
+		<?php 
+			$items = array();
+			foreach ($content['field_application_requirements']['#items'] as $item) {
+				$items[] = $item['value'];
+			}
+			print theme('item_list', array('items' => $items));
+		?>
+	</div>
+</div>
