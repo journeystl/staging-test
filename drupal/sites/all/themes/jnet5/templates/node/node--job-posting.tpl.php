e3e398df2ddb920a6fc13d1631a5297054f91a84
Jason Froderman <jason@thecatena.com>
2012-09-27 06:51:36 -0500

    Job posting changes ... round 3

--- drupal/sites/all/themes/jnet5/templates/node/node--job-posting.tpl.php
+++ drupal/sites/all/themes/jnet5/templates/node/node--job-posting.tpl.php
@@ -6,30 +6,52 @@
 ?>
 
 <div class="row">
+	<?php dpm($content); ?>
 	<div class="eight columns">
 		<div id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?>"<?php print $attributes; ?>>
 			<?php print render($title_prefix); ?>
 			<h2<?php print $title_attributes; ?>><?php print $title; ?></h2>
 			<?php print render($title_suffix); ?>
 
-			<?php
-				// We hide the comments and links now so that we can render them later.
-				// hide($content['comments']);
-				// hide($content['links']);
-				// hide($content['field_tags']);
-				// print render($content);
-				print render($content['body']);
-				// dpm($content);
-			?>
+			<?php print render($content['body']); ?>
 		</div>
 	</div>
 	<div class="four columns sidebar">
 		<div class="panel">
-			<h3>Application Requirements</h3>
+			<h4>Status: <?php print $content['field_status']['#items'][0]['value']; ?></h4>
+
+			<h4>Church: <?php print $content['field_church'][0]['#markup']; ?></h4>
+
+			<h4>Job Type</h4>
+			<?php 
+				$items = array();
+				foreach ($content['field_job_type']['#items'] as $item) {
+					if (isset($item)) {
+						$items[] = $item['value'];
+					}
+				}
+				print theme('item_list', array('items' => $items));
+			?>
+
+			<h4>Team</h4>
+			<?php 
+				$items = array();
+				foreach ($content['field_team']['#items'] as $item) {
+					if (isset($item)) {
+						$items[] = $item['value'];
+					}
+				}
+				print theme('item_list', array('items' => $items));
+			?>
+		</div>
+		<div class="panel">
+			<h4>Application Requirements</h4>
 			<?php 
 				$items = array();
 				foreach ($content['field_application_requirements']['#items'] as $item) {
-					$items[] = $item['value'];
+					if (isset($item)) {
+						$items[] = $item['value'];
+					}
 				}
 				print theme('item_list', array('items' => $items));
 			?>
