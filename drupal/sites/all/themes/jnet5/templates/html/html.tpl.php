82caaf8a13f3291d01d38a155e9d00664411fa65
Simon <syost@journeyon.net>
2012-10-02 15:03:46 -0500

    revised Google Analytics code

--- drupal/sites/all/themes/jnet5/templates/html/html.tpl.php
+++ drupal/sites/all/themes/jnet5/templates/html/html.tpl.php
@@ -24,32 +24,25 @@
   <!--[if lt IE 9]>
 		<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
 	<![endif]-->
-<?php
-  // Google Analytics -- Copyright 2010 Google Inc. All Rights Reserved.
 
-  $GA_ACCOUNT = "MO-4633630-1";
-  $GA_PIXEL = "/ga.php";
+<!-- Google Analytics -->
 
-  function googleAnalyticsGetImageUrl() {
-    global $GA_ACCOUNT, $GA_PIXEL;
-    $url = "";
-    $url .= $GA_PIXEL . "?";
-    $url .= "utmac=" . $GA_ACCOUNT;
-    $url .= "&utmn=" . rand(0, 0x7fffffff);
-    $referer = $_SERVER["HTTP_REFERER"];
-    $query = $_SERVER["QUERY_STRING"];
-    $path = $_SERVER["REQUEST_URI"];
-    if (empty($referer)) {
-      $referer = "-";
-    }
-    $url .= "&utmr=" . urlencode($referer);
-    if (!empty($path)) {
-      $url .= "&utmp=" . urlencode($path);
-    }
-    $url .= "&guid=ON";
-    return str_replace("&", "&amp;", $url);
-  }
-?>
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-4633630-1']);
+  _gaq.push(['_setDomainName', 'thejourney.org']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+
+<!-- End Google -->
 
 </head>
 <body class="<?php print $classes; ?>" <?php print $attributes;?>>
@@ -79,12 +72,7 @@
     </ul>
     <a class="close-reveal-modal">&#215;</a>
   </div>
-  
-<?php
-	// Google Analytics
-	$googleAnalyticsImageUrl = googleAnalyticsGetImageUrl();
-	echo '<img src="' . $googleAnalyticsImageUrl . '" />';
-?>
+
 
 </body>
 </html>
