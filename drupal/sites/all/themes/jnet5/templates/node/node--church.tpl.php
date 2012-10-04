e87d9feb636e71d15d1136e408e0125ee648347c
Simon <syost@journeyon.net>
2012-10-04 16:58:57 -0500

    church formating

--- drupal/sites/all/themes/jnet5/templates/node/node--church.tpl.php
+++ drupal/sites/all/themes/jnet5/templates/node/node--church.tpl.php
@@ -19,57 +19,59 @@ if (isset($location_ids[1])) {
 
 <div id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?>"<?php print $attributes; ?>>
 	<div class="row">
-		<div class="six columns">
+		<div class="six mobile-four columns">
 			<h2<?php print $title_attributes; ?>><?php print $title; ?></h2>
-			<table class="table table-condensed">
-		    <tbody>
-		    	<tr>
-						<td><strong>Services</strong></td>
-			      <td>
-			        <?php print $address_services->field_church_street[$node->language][0]['value']; ?><br>
-							<?php print $address_services->field_church_city[$node->language][0]['value']; ?>,&#32;<?php print $address_services->field_church_state[$node->language][0]['value']; ?>&#32;<?php print $address_services->field_church_zipcode[$node->language][0]['value']; ?><br>
-							<i class="foundation-location"></i>&#32;<a href="http://maps.google.com/?q=<?php print $address_services->field_church_street[$node->language][0]['value']; ?>&#32;<?php print $address_services->field_church_state[$node->language][0]['value']; ?>&#32;<?php print $address_services->field_church_zipcode[$node->language][0]['value']; ?>">Map</a>
-						</td>
-           	<td>
-	            <?php
-	           	unset($content['group_services']['#prefix']);
-	           	unset($content['group_services']['#suffix']);
-	           	$content['field_image']['#label_display'] = 'hidden';
-	            ?>
-	            <?php print render($content['group_services']); ?>
-
-	            <?php if (stristr($content['group_services']['#children'], '*')) {
-	            	print "<p class='muted smaller'>* No children's ministry provided during this service.</p>";
-	            } ?>
-           	</td>
-		      </tr>
-
-          <tr>
-          	<td colspan="3"><hr class="top"></td>
-          </tr>
-
-          <tr>
-           	<td><strong>Church Office</strong></td>
-           	<td>
-           		<?php print $address_office->field_church_street[$node->language][0]['value']; ?><br>
-							<?php print $address_office->field_church_city[$node->language][0]['value']; ?>,&#32;<?php print $address_office->field_church_state[$node->language][0]['value']; ?>&#32;<?php print $address_office->field_church_zipcode[$node->language][0]['value']; ?><br>
-							<i class="foundation-location"></i>&#32;<a href="http://maps.google.com/?q=<?php print $address_office->field_church_street[$node->language][0]['value']; ?>&#32;<?php print $address_office->field_church_state[$node->language][0]['value']; ?>&#32;<?php print $address_office->field_church_zipcode[$node->language][0]['value']; ?>">Map</a>
-           	</td>
-           	<td>
-	           	<i class="foundation-phone"></i>&#32;<?php print $field_church_phone[$node->language][0]['value']; ?><br>
-	           	<i class="foundation-mail"></i>&#32;<a href="mailto:<?php print $field_church_email[$node->language][0]['value']; ?>"><?php print $field_church_email[$node->language][0]['value']; ?></a>
-           	</td>
-	        </tr>
-				</tbody>
-			</table>
+			<div class="row">
+				<div class="four mobile-four columns">
+					<strong>Services</strong>
+				</div>
+				<div class="four mobile-two columns">
+					<?php print $address_services->field_church_street[$node->language][0]['value']; ?><br>
+												<?php print $address_services->field_church_city[$node->language][0]['value']; ?>,&#32;<?php print $address_services->field_church_state[$node->language][0]['value']; ?>&#32;<?php print $address_services->field_church_zipcode[$node->language][0]['value']; ?><br>
+												<i class="g-foundicon-location"></i>&#32;<a href="http://maps.google.com/?q=<?php print $address_services->field_church_street[$node->language][0]['value']; ?>&#32;<?php print $address_services->field_church_state[$node->language][0]['value']; ?>&#32;<?php print $address_services->field_church_zipcode[$node->language][0]['value']; ?>">Map</a>
+												
+												<a href="#">test</a>
+				</div>
+				<div class="four mobile-two columns">
+		            <?php
+		           	unset($content['group_services']['#prefix']);
+		           	unset($content['group_services']['#suffix']);
+		           	$content['field_image']['#label_display'] = 'hidden';
+		            ?>
+		            <?php print render($content['group_services']); ?>
+	
+		            <?php if (stristr($content['group_services']['#children'], '*')) {
+		            	print "<p class='muted smaller'>* No children's ministry provided during this service.</p>";
+		            } ?>
+				</div>
+				<div class="twelve columns">
+					<hr class="bottom hide-for-small">
+					<hr class="top show-for-small">
+				</div>
+			</div>
+			<div class="row">
+				<div class="four mobile-four columns">
+					<strong>Church Office</strong>
+				</div>
+				<div class="four mobile-two columns">
+	           		<?php print $address_office->field_church_street[$node->language][0]['value']; ?><br>
+								<?php print $address_office->field_church_city[$node->language][0]['value']; ?>,&#32;<?php print $address_office->field_church_state[$node->language][0]['value']; ?>&#32;<?php print $address_office->field_church_zipcode[$node->language][0]['value']; ?><br>
+		<i class="foundation-location"></i>&#32;<a href="http://maps.google.com/?q=<?php print $address_office->field_church_street[$node->language][0]['value']; ?>&#32;<?php print $address_office->field_church_state[$node->language][0]['value']; ?>&#32;<?php print $address_office->field_church_zipcode[$node->language][0]['value']; ?>">Map</a>
+				</div>
+				<div class="four mobile-two columns">
+		           	<i class="foundation-phone"></i>&#32;<?php print $field_church_phone[$node->language][0]['value']; ?><br>
+		           	<i class="foundation-mail"></i>&#32;<a href="mailto:<?php print $field_church_email[$node->language][0]['value']; ?>"><?php print $field_church_email[$node->language][0]['value']; ?></a>
+				</div>
+			</div>
 	    </div> <!--/.six columns-->
-	    <div class="six columns">
+	    <div class="six columns hide-for-small">
 	    	<?php print views_embed_view('church_photo', 'block'); ?>
 	    </div>
 
 	</div> <!-- end row -->
-
-	<hr class="top">
+	
+	<hr class="top hide-for-small">
+	<hr class="bottom show-for-small">
 
 	<div class="row">
 		<div class="twelve columns">
@@ -87,7 +89,7 @@ if (isset($location_ids[1])) {
 			$view_meet_the_staff->execute();
 			if (count($view_meet_the_staff->result) > 0) {
 				print '
-				<div class="six columns">
+				<div class="six columns hide-for-small">
 					<h3>Meet Our Staff</h3>
 					' . $view_meet_the_staff->render() . '
 				</div>
