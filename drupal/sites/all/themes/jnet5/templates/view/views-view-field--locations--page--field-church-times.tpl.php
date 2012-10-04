1a76e95af8e8b8b3de159433b3bd5408d94244c3
Simon <syost@journeyon.net>
2012-10-04 15:37:19 -0500

    added visit service time tpl

---
+++ drupal/sites/all/themes/jnet5/templates/view/views-view-field--locations--page--field-church-times.tpl.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Visit Service times - JK add
+ */
+?>
+<?php
+
+print $output;
+
+if (stristr($output, '*')) {
+	print "<p class='muted smaller'>* No children's ministry provided during this service.</p>";
+}
+
+?>
