11eea46c1e23d1a689f70d717e3e804ce3432c6c
Charlie Schliesser <charlie.schliesser@gmail.com>
2012-11-15 09:22:19 -0600

    Added test-worker.php file in document root.

--- drupal/test-worker.php
+++ drupal/test-worker.php
@@ -1,11 +1,11 @@
 <?php
 
 // Bootstrap Drupal
-define("DRUPAL_ROOT", dirname(__FILE__) . '/../drupal');
+define("DRUPAL_ROOT", dirname(__FILE__));
 $_SERVER['REMOTE_ADDR'] = '127.0.0.1';
 $_SERVER['REQUEST_METHOD'] = 'GET';
 $_SERVER['SERVER_SOFTWARE'] = 'Apache/2.2.16 (Ubuntu)';
-require_once dirname(__FILE__) . '/../drupal/includes/bootstrap.inc';
+require_once dirname(__FILE__) . '/includes/bootstrap.inc';
 drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
 
 while (1) {
