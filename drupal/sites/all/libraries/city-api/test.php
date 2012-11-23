51a29e5e6679e6c30d238cf681ddf0c3997aa3a7
Jason Froderman <jason@thecatena.com>
2012-11-23 16:31:48 -0600

    CG map work

--- drupal/sites/all/libraries/city-api/test.php
+++ drupal/sites/all/libraries/city-api/test.php
@@ -5,6 +5,19 @@
     	// $ca->debug = true;
 		$ca->json = true;
 
-		$results = $ca->groups_index(array('group_types' => "CG")); 
-    	echo $results;
+		// Get campus_name
+		$groups_index_results = $ca->groups_index(array('group_types' => "CG")); 
+
+		// groups_show (For dates?)
+		// groups_addresses_show (For geocoding -- to zip code or?)
+		// groups_tags_index (For type)
+		// groups_roles_index (For leader last name / email address?)
+    	echo $groups_index_results;
+
+    	echo "---<br />---<br />";
+
+    	// 24434 = Overflow group
+    	$groups_show_results = $ca->groups_show(24434);
+    	echo $groups_show_results;
+
 ?>
