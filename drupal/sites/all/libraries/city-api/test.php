db43b31b35e090a7911fe721bc7ce4ec5c146a66
Jason Froderman <jason@thecatena.com>
2012-11-24 12:52:09 -0600

    CG map work ... round 4

--- drupal/sites/all/libraries/city-api/test.php
+++ drupal/sites/all/libraries/city-api/test.php
@@ -20,13 +20,53 @@
     	// $groups_show_results = $ca->groups_show(24434);
     	// echo $groups_show_results;
 
+		// Do we ever have more than 1 address, if so FLAG IT!
+		/* {
+		    "total_pages": 1,
+		    "per_page": 20,
+		    "current_page": 1,
+		    "addresses": [
+		        {
+		            "location_type": "Host",
+		            "city": "St. Louis",
+		            "street": "7701 Maryland Ave",
+		            "latitude": 38.651321,
+		            "group_external_id": "",
+		            "updated_at": "09/07/2012 08:29 PM (GMT)",
+		            "street2": "",
+		            "group_id": 24434,
+		            "created_at": "09/03/2010 03:48 PM (GMT)",
+		            "zipcode": "63105",
+		            "longitude": -90.333914,
+		            "id": 93886,
+		            "state": "MO",
+		            "privacy": "Private"
+		        }
+		    ],
+		    "total_entries": 1
+		} */
     	$groups_addresses_index = $ca->groups_addresses_index(24434);
     	echo $groups_addresses_index;
 
     	$groups_tags_results = $ca->groups_tags_index(24434);
     	echo $groups_tags_results;
 
-    	$groups_roles_results = $ca->groups_roles_index(24434);
+    	// Search for title: leader
+    	// If we don't have one, flag it.
+    	/* {
+		    "roles": [
+		    	{
+		            "user_type": "User",
+		            "last_engaged": "10/30/2012",
+		            "user_name": "Kevin Frank",
+		            "active": true,
+		            "user_api_url": "https://api.onthecity.org/users/56346",
+		            "created_at": "09/09/2012",
+		            "title": "Leader",
+		            "id": 2516013,
+		            "user_id": 56346
+		        }, */
+    	$groups_roles_results = $ca->groups_roles_index(24434, array('title' => "Leader"));
     	echo $groups_roles_results;
 
 
