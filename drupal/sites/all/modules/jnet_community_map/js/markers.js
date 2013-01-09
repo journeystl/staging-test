a46f1b9add404d8798b3cd2927d588d711d6a2f8
Kraig Hufstedler <kraigory@gmail.com>
2013-01-09 12:08:11 -0600

    css and js changes

--- drupal/sites/all/modules/jnet_community_map/js/markers.js
+++ drupal/sites/all/modules/jnet_community_map/js/markers.js
@@ -303,7 +303,7 @@
         "<h1 class='" + titleStatus + "'>" + marker.title + "</h1>" +
         "<h2 class='" + churchStatus + "'>Home Church: " + markers_raw[m_id].campus.replace(/^\s+|\s+$/g,'') + "</h2>" +
         "<h2 class='" + typeStatus + "'>Type: " + typeGroups[marker.typeIndex] + " " + kidFriendlyString + "</h2>" +
-        "<div class='dayBox'><span class='" + dayChecker(1) +  "'>M</span><span class='" + dayChecker(2) +  "'>T</span><span class='" + dayChecker(3) +  "'>W</span><span class='" + dayChecker(4) +  "'>R</span><span class='" + dayChecker(5) +  "'>F</span><span class='" + dayChecker(6) +  "'>S</span><span class='" + dayChecker(7) +  "'>S</span></div>"+
+        "<div class='dayBox label radius secondary'><span class='" + dayChecker(1) +  "'>M</span><span class='" + dayChecker(2) +  "'>T</span><span class='" + dayChecker(3) +  "'>W</span><span class='" + dayChecker(4) +  "'>R</span><span class='" + dayChecker(5) +  "'>F</span><span class='" + dayChecker(6) +  "'>S</span><span class='" + dayChecker(7) +  "'>S</span></div>"+
         "<ul class='button-group radius'><li><a href='#' onclick='joinGroup(\""+
               marker.leaderEmail + "\",\"" + marker.id + "\",\"" + marker.title + "\",\"" + returnType(marker.typeIndex) +
               "\",\"" + marker.kidFriendly + "\",\""+ daysString +
