a1594abc257cbc1501fe320313504d49a454a5f9
Kraig Hufstedler <kraigory@gmail.com>
2013-01-11 11:40:57 -0600

    styling report email

--- drupal/sites/all/modules/jnet_community_map/js/email-popups.js
+++ drupal/sites/all/modules/jnet_community_map/js/email-popups.js
@@ -33,11 +33,11 @@
 
     jQuery("#reportErrorPopup").reveal();
 
-    jQuery("#edit-leader-email").val(leaderEmail);
-    jQuery("#edit-group-id").val(groupID);
-    jQuery("#edit-group-name").val(groupName);
-    jQuery("#edit-group-kids").val(kidFriendly);
-    jQuery("#edit-group-days").val(meetingDays);
+    jQuery("#edit-leader-email--2").val(leaderEmail);
+    jQuery("#edit-group-id--2").val(groupID);
+    jQuery("#edit-group-name--2").val(groupName);
+    jQuery("#edit-group-kids--2").val(kidFriendly);
+    jQuery("#edit-group-days--2").val(meetingDays);
 
     // // Set action - GET variables
     // var phpScript = "send_email.php";
