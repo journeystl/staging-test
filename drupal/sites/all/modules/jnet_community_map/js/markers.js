d5e3d99713fcdf2398ce52bae62792ab887ac282
Kraig Hufstedler <kraigory@gmail.com>
2013-02-01 13:19:00 -0600

    removed church type from popup

--- drupal/sites/all/modules/jnet_community_map/js/markers.js
+++ drupal/sites/all/modules/jnet_community_map/js/markers.js
@@ -313,8 +313,8 @@ var markers = [];
         "<h1 class='" + titleStatus + "'>" + marker.title + "</h1>" +
         "<h2 class='church-label " + churchStatus + "'>" + markers_raw[m_id].campus.replace(/^\s+|\s+$/g,'') + "</h2>" +
         "<div class='dayBox label radius secondary'><span class='" + dayChecker(1) +  "'>M</span><span class='" + dayChecker(2) +  "'>T</span><span class='" + dayChecker(3) +  "'>W</span><span class='" + dayChecker(4) +  "'>R</span><span class='" + dayChecker(5) +  "'>F</span><span class='" + dayChecker(6) +  "'>S</span><span class='" + dayChecker(7) +  "'>S</span></div>"+
-        "<hr>" +
-        "<h2 class='" + typeStatus + "'>" + typeGroups[marker.typeIndex] + kidFriendlyString + "</h2>" +
+        // "<hr>" +
+        // "<h2 class='" + typeStatus + "'>" + typeGroups[marker.typeIndex] + kidFriendlyString + "</h2>" +
         "<ul class='button-group radius'><li><a href='javascript:;' onclick='groupAction(\""+
               m_id + "\",\"" + marker.title + "\",\"" + returnType(marker.typeIndex) +
               "\",\"" + marker.kidFriendly + "\",\"" + daysString + "\",\"join\",\"" + plainChurchName + "\")' class='joinGroup button radius small'>Request to Join</a></li>"+
