bfd067867aec9bccf134cc06f6f9eaefb922eb8d
Jason Froderman <jason@thecatena.com>
2013-02-01 18:45:09 -0600

    dpm for search results

--- drupal/sites/all/themes/jnet5/templates/search/search-results.tpl.php
+++ drupal/sites/all/themes/jnet5/templates/search/search-results.tpl.php
@@ -22,6 +22,7 @@
  */
 ?>
 <?php if ($search_results): ?>
+  <?php dpm($search_results); ?>
   <h2><?php print t('Search results');?></h2>
   <ol class="search-results <?php print $module; ?>-results">
     <?php print $search_results; ?>
