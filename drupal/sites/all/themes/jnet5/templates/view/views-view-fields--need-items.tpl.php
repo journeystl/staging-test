c253dd32e7eb410f51bf2f3abc90c3336cd88ec4
Kraig Hufstedler <kraigory@gmail.com>
2013-02-07 14:50:56 -0600

    minor fixes

--- drupal/sites/all/themes/jnet5/templates/view/views-view-fields--need-items.tpl.php
+++ drupal/sites/all/themes/jnet5/templates/view/views-view-fields--need-items.tpl.php
@@ -1,10 +1,10 @@
 
-<span class="need-item"><?php print $fields['field_need_item_description']->content; ?></span>
+<span class="need-item three columns"><?php print $fields['field_need_item_description']->content; ?></span>
 <?php
   if (!empty($fields['field_need_item_author_longname']->content)) {
-    $action = '<a href="javascript:;" class="button tiny radius secondary disabled">' . $fields['field_need_item_author_longname']->content . '</a>';
+    $action = '<span class="one column"><a href="javascript:;" class="button tiny radius secondary disabled">' . $fields['field_need_item_author_longname']->content . '</a></span>';
   } else {
-    $action = '<a href="' . $fields['field_short_url']->content . '" class="button tiny radius">I Can Help!</a>';
+    $action = '<span class="one column"><a href="' . $fields['field_short_url']->content . '" class="button tiny radius">I Can Help!</a></span>';
   }
   print $action;
 ?>
